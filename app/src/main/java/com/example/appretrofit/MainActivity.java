9d1d9abf6c641e778e496e5613a78a9fa50c0d56
Pablo-Silva <Pablo-Silva>
2019-05-13 11:16:38 -0300

    Passando Void ao inves de Filme

--- app/src/main/java/com/example/appretrofit/MainActivity.java
+++ app/src/main/java/com/example/appretrofit/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.appretrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -59,9 +57,12 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), json);
 
 
-        mAPIService.savePost(body).enqueue(new Callback<Filme>() {
+        mAPIService.savePost(body).enqueue(new Callback<Void>() {
+
             @Override
-            public void onResponse(Call<Filme> call, Response<Filme> response) {
+            public void onResponse(Call<Void> call, Response<Void> response) {
+
+                showResponse(response.body().toString());
 
                 if(response.isSuccessful()) {
                     showResponse(response.body().toString());
@@ -71,10 +72,11 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<Filme> call, Throwable t) {
+            public void onFailure(Call<Void> call, Throwable t) {
                 Toast.makeText(getApplicationContext(), "Unable to submit post to API." , Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     public void showResponse(String response) {
